d996b4ce8a6f90d65a7cf03ff041b6736bda7c0c
Matt Elston <m.elston@ufl.edu>
2025-05-22 17:20:17 -0700

    added JS code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function greet() {
+    const name = prompt("What is your name?");
+        alert("Hello, " +name+ "!");
+
+    const age = prompt("So, how old are you " +name+ "?");
+    const agenum = parseInt(age);
+
+    const birthday = confirm("Have you had a birthday yet this year?")
+    const currentyear = new Date().getFullYear();
+    
+    if (birthday) {
+        birthyear = currentyear - agenum;
+        alert("Your birthday has passed, you were born in " +birthyear+ ".");
+    } else {
+        birthyear = currentyear - agenum - 1;
+        alert("Your birthday has NOT passed, you were born in " +birthyear+ ".");
+    }
+}
